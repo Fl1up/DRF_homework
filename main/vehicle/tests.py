57d60a29df34453c551db7c97b3ddd8f52b918f5
German <germanstarinenko@gmail.com>
2023-08-17 14:13:09 +0300

    append payment

--- main/vehicle/tests.py
+++ main/vehicle/tests.py
@@ -3,7 +3,7 @@ from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 from main.users.models import User
-from main.vehicle.models import Course, Lesson, Subscription
+from main.vehicle.models import Course, Subscription, Pay, Lessons
 from main.vehicle.serializers import SubscriptionSerializer
 
 
@@ -11,9 +11,8 @@ class LessonTestsCase(APITestCase):
     def setUp(self):
         user = User.objects.create(email='testuser@mail.ru', password='testpass')
         self.course = Course.objects.create(title='Test Course', description='Test Description', owner=user)
-        self.create_lesson = Lesson.objects.create(title='Вводный урок', course=self.course, url_video='https://www.youtube.com/@skypro-917')
-
-
+        self.create_lesson = Lessons.objects.create(title='Вводный урок', course=self.course,
+                                                   url_video='https://www.youtube.com/@skypro-917')
 
     def test_create_lesson(self):
         response = self.client.post(
@@ -27,14 +26,14 @@ class LessonTestsCase(APITestCase):
         return response
 
     def test_retrieve_lesson(self):
-        lesson = Lesson.objects.create(title='Вводный урок', course=self.course, url_video='https://www.youtube.com/@skypro-917')
+        lesson = Lessons.objects.create(title='Вводный урок', course=self.course,
+                                       url_video='https://www.youtube.com/@skypro-917')
         response = self.client.get(f'/Lesson/{lesson.id}/')
         self.assertEqual(response.status_code, 200)
 
-
     def test_update_lesson(self):
         course = Course.objects.create(title='Some course')
-        lesson = Lesson.objects.create(title='Some title', description='Some description', course=course)
+        lesson = Lessons.objects.create(title='Some title', description='Some description', course=course)
         response = self.client.patch(f'/Lesson/update/{course.id}/',
                                      {'title': '1 урок', 'course': course.id,
                                       'video': 'https://www.youtube.com/@skypro-916'}
@@ -58,18 +57,85 @@ class CourseSubscriptionTestCase(APITestCase):
         )
         return response
 
-    # def test_delete_subscription(self):  # не видит unsubscribe
-    #     url = reverse('unsubscribe', args=[self.course.id, self.subscription.id])
-    #     response = self.client.delete(url)
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertFalse(CourseSubscription.objects.filter(id=self.subscription.id).exists())
-    #
-    # def test_retrieve_subscription(self): # не видит subscribe-get
-    #     url = reverse("subscribe-get", args=[self.subscription.id])
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data['email'], 'test@test.com')
 
+class PayTestCase(APITestCase):
+    def setUp(self) -> None:
+        """Общие данные"""
 
+        self.user = User.objects.create(
+            email="3@admin.ru", password="123", is_active=True
+        )
+        self.user.save()
+        self.client.force_authenticate(user=self.user)
+        self.pay_test = Pay.objects.create(
+            id=2,
+            pay_date='2099-08-06"',
+            pay_sum=15,
+            payment_type="CASH",
+            user=self.user,
+        )
 
+    def test_create_pay(self):
+        """Создание курса тест"""
 
+        data = {
+            "id": 1,
+            "pay_sum": 2000,
+            "payment_type": "Card",
+            "user": 1,
+            "course_name": 1
+        }
+
+        response = self.client.post("/payment/create/", data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Pay.objects.all().exists())  # нахождение в базе
+
+    def test_list_pay(self):
+        """Вывод всех оплат тест"""
+        responce = self.client.get("/payment/list/")
+
+        data = {
+            {
+                "count": 3,
+                "next": None,
+                "previous": None,
+                "results": [
+                    {
+                        "id": 1,
+                        "pay_date": "2023-08-17",
+                        "pay_sum": 2500,
+                        "payment_type": "Card",
+                        "user": 1,
+                        "course_name": 1,
+                        "lesson": 1
+                    },
+                ],
+            }
+        }
+
+        self.assertEqual(responce.status_code, status.HTTP_200_OK)
+        self.assertEqual(responce.json(), data)
+
+    def test_detail_pay(self):
+        """Вывод одной оплаты тест"""
+        response = self.client.get("/payment/deteil/2/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_pay(self):
+        """Тест обновления оплаты"""
+        data = {
+            "pay_sum": 150,
+            "pay_date": "2023-08-12",
+            "payment_type": "CASH",
+            "user": self.user.id,
+        }
+
+        response = self.client.put("/payment/update/2/", data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_pay(self):
+        """Тест удаления оплаты"""
+        response = self.client.delete("/payment/delete/2/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
